0f02a39f5a32187ebaab7b6a00a84dbe34fc27eb
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-05-04 18:07:26 +0300

    review slick slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,22 @@
+$(document).ready(function(){
+    $('.review__inner').slick({
+        slidesToShow: 3,
+        centerMode: true,
+        speed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev-arrow.png" alt="prev-arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/next-arrow.png" alt="next-arrow"></button>',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1
+              }
+            }
+        ]
+    });
+  });
+
+
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.promo__menu'),
     menuItem = document.querySelectorAll('.promo__menu-item'),
